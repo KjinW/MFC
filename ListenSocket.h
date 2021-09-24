9932fe65769e5416d3cbe0857428d901e39f2524
KjinW <kjw4563@naver.com>
2021-09-24 15:28:12 +0900

    TCP/IP Server

---
+++ ListenSocket.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <afxsock.h>
+class CListenSocket :
+	public CAsyncSocket
+{
+
+public:
+
+	CListenSocket();
+	virtual ~CListenSocket();
+
+	CPtrList m_ptrClientSocketList;
+
+
+	virtual void OnAccept(int nErrorCode);
+	void CloseClientSocket(CSocket* pClient);
+	void SendChatDataAll(TCHAR* pszMessage);
+};
+
